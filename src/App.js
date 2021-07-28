55c76811e1f7bc46ad35f136acc114a4f22e2b68
Dario Mujica <dariomdq94@gmail.com>
2021-07-27 22:33:50 -0300

    desafio1.2

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import Test from './components/NavBar'
+
+function App() {
+  return (
+    <>
+    <Test/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+export default function Button() {
+  return (
+    <>
+      <div class="topnav">
+        <a class="active" href="#home">
+          MINARTE
+        </a>
+        <a href="#catalogo">CATALOGO</a>
+        <a href="#housing">HOUSING</a>
+        <a href="#nosotros">NOSOTROS</a>
+      </div>
+    </>
+  );
+}
